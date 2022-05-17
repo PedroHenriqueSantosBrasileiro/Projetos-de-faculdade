2ae64ece86c6175772698a7af060f612a8300c30
PedroHenriqueSantosBrasileiro <psantosbrasileiro1@gmail.com>
2022-05-17 11:11:40 -0300

    criação da classe horaextra

---
+++ Holerite/src/holerite/HoraExtra.java
@@ -0,0 +1,14 @@
+package holerite;
+
+
+public class HoraExtra {
+    
+double salarioBrutoMaisHoraExtra;
+
+public double CalculoHoraExtra (double x, double y){
+    salarioBrutoMaisHoraExtra = x + y;
+    
+    return salarioBrutoMaisHoraExtra;
+}
+    
+}
--- Holerite/src/holerite/Principal.java
+++ Holerite/src/holerite/Principal.java
@@ -15,8 +15,9 @@ public class Principal {
         ValeRefeição valeRefeição = new ValeRefeição();
         ValeAlimentação valeAlimentação = new ValeAlimentação();
         Convenio convenio = new Convenio();
+        HoraExtra horaextra = new HoraExtra ();
 
-        double salarioBruto, salarioLiquido, salarioBrutoMenosInss;
+        double salarioBruto,horaExtra, salarioLiquido, salarioMenosInss;
         String nome;
 
         System.out.println("Insira o nome do funcionário: ");
@@ -24,14 +25,22 @@ public class Principal {
 
         System.out.println("Insira o salário do Funcionário: ");
         salarioBruto = tc.nextDouble();
+        
+        System.out.println("Informe o valor de horas extras: ");
+        horaExtra = tc.nextDouble();
+        horaextra.CalculoHoraExtra(horaExtra, salarioBruto);
+        
+        
 
         inss.CalculoDescontoInss(salarioBruto);
-        salarioBrutoMenosInss = salarioBruto - inss.CalculoDescontoInss(salarioBruto);
+        salarioMenosInss = horaextra.salarioBrutoMaisHoraExtra - inss.CalculoDescontoInss(horaextra.salarioBrutoMaisHoraExtra);
 
         System.out.println(nome);
-        System.out.printf("Inss = %.2f%n", inss.CalculoDescontoInss(salarioBruto));
+        System.out.printf("Salario: %.2f%n",salarioBruto);
+        System.out.printf("Hora Extra; %.2f%n",horaextra.salarioBrutoMaisHoraExtra);
+        System.out.printf("Inss = %.2f%n", inss.CalculoDescontoInss(horaextra.salarioBrutoMaisHoraExtra));
         System.out.printf("Vt = %.2f%n", valeTransporte.CalculoDescontoValeTransporte(salarioBruto));
-        System.out.printf("Ir = %.2f%n", irrf.CalculoDescontoIrrf(salarioBrutoMenosInss));
+        System.out.printf("Ir = %.2f%n", irrf.CalculoDescontoIrrf(salarioMenosInss));
         System.out.printf("Vr = %.2f%n", valeRefeição.CalculoDescontoValeRefeição(salarioBruto));
         System.out.printf("VA = %.2f%n", valeAlimentação.CalculoValeAlimentação(salarioBruto));
         System.out.printf("Convenio = %.2f%n", convenio.CalculoDescontoConvenio(salarioBruto));
